f78f790025870233c7cc2ac7e498e926827d29ed
moussaabbou <66470514+moussaabou@users.noreply.github.com>
2024-09-25 14:04:23 +0200

    change

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesFlowers52093861280
+  /// assets/images/flowers-5209386_1280.jpg
+  static const String imagesFlowers52093861280 = "assets/images/flowers-5209386_1280.jpg";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = Color.fromARGB(160, 13, 183, 30);
+  static Color offWhite = const Color(0xffF8F4F9);
+}
